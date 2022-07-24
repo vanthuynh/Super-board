763df802ac6e2695a244e21ca3fad5bbb4bc5d01
Van Huynh <vthuynh@cpp.edu>
2022-07-24 02:08:29 -0400

    remove ESlint Error

--- src/components/ThemeSettings.jsx
+++ src/components/ThemeSettings.jsx
@@ -38,7 +38,6 @@ const ThemeSettings = () => {
               onChange={setMode}
               checked={currentMode === "Light"}
             />
-            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
             <label htmlFor="light" className="ml-2 text-md cursor-pointer">
               Light
             </label>
@@ -53,7 +52,6 @@ const ThemeSettings = () => {
               className="cursor-pointer"
               checked={currentMode === "Dark"}
             />
-            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
             <label htmlFor="dark" className="ml-2 text-md cursor-pointer">
               Dark
             </label>
