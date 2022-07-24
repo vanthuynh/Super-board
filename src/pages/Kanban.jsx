93ad55d702b1e442f33df89af529c94709239e5b
Van Huynh <vthuynh@cpp.edu>
2022-07-23 23:05:35 -0400

    add structure for Kanban board

--- src/pages/Kanban.jsx
+++ src/pages/Kanban.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import {
+  KanbanComponent,
+  ColumnsDirective,
+  ColumnDirective,
+} from "@syncfusion/ej2-react-kanban";
 
-const Kanban = () => {
-  return <div>Kanban</div>;
-};
+import { kanbanData, kanbanGrid } from "../data/dummy";
+import { Header } from "../components";
+
+const Kanban = () => (
+  <div className="">
+    <Header category="App" title="Kanban" />
+    <KanbanComponent
+      id="kanban"
+      keyField="Status"
+      dataSource={kanbanData}
+      cardSettings={{ contentField: "Summary", headerField: "Id" }}
+    ></KanbanComponent>
+  </div>
+);
 
 export default Kanban;
