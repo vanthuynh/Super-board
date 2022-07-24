633066c28c6adea1b0d52a63d974eb49012e8e3c
Van Huynh <vthuynh@cpp.edu>
2022-07-24 01:58:41 -0400

    import necessary package for color picker

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -10,13 +10,13 @@ import {
 } from "@syncfusion/ej2-react-richtexteditor";
 
 import { Header } from "../components";
-import { EditorData } from "../data/dummy";
+// import { EditorData } from "../data/dummy";
 
 const Editor = () => (
   <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
     <Header category="App" title="Editor" />
     <RichTextEditorComponent>
-      <EditorData />
+      {/* <EditorData /> */}
       <Inject services={[HtmlEditor, Toolbar, Image, Link, QuickToolbar]} />
     </RichTextEditorComponent>
   </div>
