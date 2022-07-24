465f7e875b08dc96e3a15437ecd7f998502e61b1
Van Huynh <vthuynh@cpp.edu>
2022-07-23 23:03:33 -0400

    finish implementing calendar

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -34,7 +34,42 @@ const Scheduler = () => {
   };
 
   return (
-    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl"></div>
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="App" title="Calendar" />
+      <ScheduleComponent
+        height="650px"
+        ref={(schedule) => setScheduleObj(schedule)}
+        selectedDate={new Date(2021, 0, 10)}
+        eventSettings={{ dataSource: scheduleData }}
+        dragStart={onDragStart}
+      >
+        <ViewsDirective>
+          {["Day", "Week", "WorkWeek", "Month", "Agenda"].map((item) => (
+            <ViewDirective key={item} option={item} />
+          ))}
+        </ViewsDirective>
+        <Inject
+          services={[Day, Week, WorkWeek, Month, Agenda, Resize, DragAndDrop]}
+        />
+      </ScheduleComponent>
+      <PropertyPane>
+        <table style={{ width: "100%", background: "white" }}>
+          <tbody>
+            <tr style={{ height: "50px" }}>
+              <td style={{ width: "100%" }}>
+                <DatePickerComponent
+                  value={new Date(2021, 0, 10)}
+                  showClearButton={false}
+                  placeholder="Current Date"
+                  floatLabelType="Always"
+                  change={change}
+                />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </PropertyPane>
+    </div>
   );
 };
 
