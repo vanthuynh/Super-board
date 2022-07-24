44a5ea3a05cd70f04fe6f8e793a8ffa0e83c75ba
Van Huynh <vthuynh@cpp.edu>
2022-07-24 01:55:05 -0400

    import necessary components for editor

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -1,4 +1,16 @@
 import React from "react";
+import {
+  HtmlEditor,
+  Image,
+  Inject,
+  Link,
+  QuickToolbar,
+  RichTextEditorComponent,
+  Toolbar,
+} from "@syncfusion/ej2-react-richtexteditor";
+
+import { Header } from "../components";
+import { EditorData } from "../data/dummy";
 
 const Editor = () => {
   return <div>Editor</div>;
